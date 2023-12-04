56e1530fb10b08546d3561fba6e91b96b8dc7834
sumanbro0 <pandey.suman258@gmail.com>
2023-12-04 20:14:21 +0545

    ui

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ class Manager:
         u=next((usr for usr in self.users_manager.users if str(usr["user_id"])==user),None) 
         if u:
             self.user=u
+    
+    def show_help(self):
+        print("\nHelp - Library Management System:".center(50,"-"))
+        print(" 'au': Adds a new user to the system.")
+        print(" 'vu': Displays details of all users.")
+        print(" 'su': Displays details of single user.")
+        print(" 'ab': Adds books to the library.")
+        print(" 'vb': Displays details of all books in the library")
+        print(" 'b': Allows a user to borrow a book.")
+        print(" 'r': Allows a user to return a borrowed book.")    
+        print(" 's': Selects a user to perform actions.")
+        print(" 'u': Upgrades a user to a special member.")
+        print(" 'h': Displays this help menu.")
+        print(" 'e': Exits the system.")
+        print(" 'rep': View the entire report.")
 
     def run_library_system(self):
         while True:
@@ -39,6 +54,7 @@ class Manager:
                     
                 case "u":
                     self.users_manager.upgrade_user()
+                    
                 case "r":
                     if self.user:            
                         self.library.return_books(self.user)
@@ -52,6 +68,7 @@ class Manager:
                     else:
                         print("Select User to operate on")
                         self.select_student()
+                
                 case "e":
                     print("Saving data")
                     self.library.save_books_to_file()
@@ -59,8 +76,12 @@ class Manager:
                     print("Exitting...")
                     break
                 
+                case "h":
+                    self.show_help()
+
                 case _:
                     print("Invalid choice")
+                    self.show_help()
 
 
 Manager().run_library_system()
